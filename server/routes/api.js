a6bbd967fc16f6125e63dd9341d2f74e3dc522ef
Deepak Surendran <deepak@ceegees.in>
2018-08-19 01:57:53 +0530

    adding register and update api for mobile app

--- server/routes/api.js
+++ server/routes/api.js
@@ -421,9 +421,121 @@ router.post('/add-safe-user',function(req,res){
     } catch(ex){
         console.log(ex);
         res.json(jsonError('Missing parameters' , {...req.body}) );
-    }
-        
+    }   
 });
+
+
+router.post('/rescue/volunteer/register', function(req,res) {
+    let name = req.body.name;
+    let phoneNumber = req.body.phoneNumber
+    let type = req.body.type;
+    let latitude = req.body.latitude;
+    let longitude = req.body.longitude;
+    let info = req.body.info;
+
+    if (!name) {
+        return res.json({
+            success: false,
+            message: "Name is required"
+        });
+    } else if (!phoneNumber) {
+        return res.json({
+            success: false,
+            message: "Phone Number is required"
+        });
+    } else if (!type) {
+        return res.json({
+            success: false,
+            message: "Rescue Type is required"
+        });
+    } else if (!latitude || !longitude) {
+        return res.json({
+            success: false,
+            message: "Location detail is required."
+        });
+    }
+
+    models.RescueVolunteer.findOrCreate({
+        where: {
+            phoneNumber: phoneNumber
+        },
+        defaults: {
+            name: name,
+            type: type,
+            latitude: latitude,
+            longitude: longitude,
+            info: info
+        }
+    }).spread((resVol, created) => {
+        if (! created) {
+            resVol.set('name', name);
+            resVol.set('type', type);
+            resVol.set('latitude', latitude);
+            resVol.set('longitude', longitude);
+            return resVol.save();
+        } else {
+            return resVol;
+        }
+    }).then(data => {
+        res.json({
+            success: true,
+            message: "Added Successfully."
+        });
+    }).catch(err => {
+        res.json({
+            success: false,
+            message: "Something went wrong. Please try again."
+        });
+    });
+});
+
+
+router.post('/rescue/volunteer/status/update', function(req,res) {
+    let phoneNumber = req.body.phoneNumber;
+    let latitude = req.body.latitude;
+    let longitude = req.body.longitude;
+    let status = req.body.status;
+
+    if (!phoneNumber) {
+        return res.json({
+            success: false,
+            message: "Phone Number is required"
+        });
+    } else if (!latitude || !longitude) {
+        return res.json({
+            success: false,
+            message: "Location detail is required."
+        });
+    }
+
+    models.RescueVolunteer.find({
+        where: {
+            phoneNumber: phoneNumber
+        }
+    }).then(resVol => {
+        if (!resVol) {
+            throw "Rescue Volunteer not found";
+        }
+        return resVol.update({
+            'latitude': latitude,
+            'longitude': longitude,
+            'status': (status)?'ACTIVE':'INACTIVE'
+        });
+    }).then(result => {
+        res.json({
+            success: true,
+            message: "Data updated."
+        });
+    }).catch(err => {
+        res.json({
+            success: false,
+            message: err
+        });
+    });
+});
+
+
+
 module.exports = router;
 
 
