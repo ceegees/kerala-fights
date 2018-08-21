c23c6f8ee88473dfe468ae5f4dc876a4da52aeaf
Sajith <sajith@ceegees.in>
2018-08-22 00:19:27 +0530

    Integrating FB Login

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,9 +12,13 @@ const initialState = require('../config/data');
   
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 GOOGLE_APP_ID = '634343394363-lgpiebfiddkac72v25ao1ld1b3tnre8i.apps.googleusercontent.com';
 GOOGLE_APP_SECRET = 'Z0KAE4ZU2rnX5kt0qSVsbm4T'
+
+FACEBOOK_APP_ID = '756308454761013';
+FACEBOOK_APP_SECRET = '27525342c32b054d7056fcdeb6f072f3';
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_APP_ID,
@@ -26,8 +30,7 @@ passport.use(new GoogleStrategy({
         'https://www.googleapis.com/auth/user.phonenumbers.read'
     ],
     callbackURL: "/auth/google-callback"
-  },
-  function(accessToken, refreshToken, profile, cb) { 
+  },function(accessToken, refreshToken, profile, cb) { 
     const refId = `https://plus.google.com/${profile.id}`;
     models.User.findOne({
         where:{
@@ -66,12 +69,63 @@ router.get('/auth/google-callback', passport.authenticate('google',{
 
 router.get('/auth/google', passport.authenticate('google'));
 
-function googleAuth(req,res,next){
+function googleAuth(req,res,next) {
     if (req.session.passport && req.user )  {
         next();
     }
     res.redirect('/auth/google'); 
-} 
+}
+
+// **** Facebook Auth **** //
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "/auth/facebook/callback",
+    profileFields: ['id', 'emails', 'displayName','name','gender', 'picture.type(small)'] //This
+},
+
+function(accessToken, refreshToken, profile, done) {
+        const refId = `https://facebook.com/${profile.id}`;
+        
+        console.log('fb user ',profile);
+        models.User.findOne({
+            where: {
+                provider_id: refId
+            }
+        }).then(resp => { 
+            if (resp) {
+                return resp;
+            }
+
+            let  photoLink = null;
+            if (profile.photos.length > 0){
+                photoLink = profile.photos[0].value;
+            }
+          
+            return models.User.create({
+                name: profile.displayName,
+                providerId: refId,
+                type: 'User',
+                email: profile.emails[0].value,
+                profileLink: photoLink,
+                status:'ACTIVE'
+            });
+        }).then(user => {
+            console.log('at second then', user);
+            done(null, user);
+        }).catch(ex => {
+            done(null, false);
+            console.log('Error', ex);
+        });
+    }
+));
+
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/?failed_login=true',
+    successRedirect: '/manage', 
+}));
 
 //SSR function import
 const ssr = require('./../views/ssr.js'); 
